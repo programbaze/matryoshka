f8c24a299b137945c8fb47a8a856ec05bc80140a
makarov.m <makarov.m@tm.biz-apps.ru>
2023-10-08 14:21:30 +0300

    animate folders

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 $(document).on("click",".js--matryoshka-btn",function() {
-  $(this).toggleClass('matryoshka__btn_minus');
+  $(this).toggleClass('matryoshka__btn_plus');
+  $(this).next('.js--matryoshka-folder').toggleClass('matryoshka__folder_close');
   $(this).closest('.js--matryoshka-item').children('.js--matryoshka-content').toggleClass('matryoshka__content_hide');
 });
 
